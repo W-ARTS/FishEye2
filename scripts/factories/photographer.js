98e0c4bb45a797c99bfe3a3b3c7c19b1c42343e3
Wael <you@example.com>
2023-01-27 16:50:51 +0100

    ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, city, country, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
     
@@ -7,18 +7,27 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         const h3 = document.createElement( 'h3' );
-        const h3C = document.createElement('h3');
-        h3.textContent = city, country;
+        const h3_2 = document.createElement('h3');
+        const p = document.createElement( 'p' );
+        const p_2 = document.createElement('p');
+        const prix = document.createElement('p');
+        prix.innerHTML = "/jours";
+        h3.textContent = city;
+        h3_2.textContent = country;
+        p.textContent = tagline;
+        p_2.textContent = price, prix;
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
-        article.appendChild(h3);
+        article.appendChild(h3_2);
+        article.appendChild(p);
+        article.appendChild(p_2);
         
         return (article);
     }
-    return { name, picture, city, country, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
 
--- scripts/pages/pages.js
+++ scripts/pages/pages.js
@@ -1,4 +1,5 @@
 //Evenement du clique photographers
+
 const photoUsers = document.getElementsByClassName('photographer_section')[0];
 photoUsers.addEventListener('click', () => {
     window.location.href = "/photographer.html?id=243";
