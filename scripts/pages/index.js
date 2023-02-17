851d71bc55a48ec8c88a9cc1cc957f7539265157
Wael <you@example.com>
2023-02-17 17:39:51 +0100

    ok

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -27,7 +27,6 @@
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
-        console.log(photographers);
         displayData(photographers);
     };
 
